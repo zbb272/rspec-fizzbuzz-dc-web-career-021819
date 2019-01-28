ad3d97acd3b4206288d8df1c801957352fee8dea
Zachary <zbberman@yahoo.com>
2019-01-28 21:49:16 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz (int)
   if int % 3 == 0 && int % 5 == 0
     "FizzBuzz"
-  else if int % 3 == 0
+  elsif int % 3 == 0
     "Fizz"
-  # else if int % 5 == 0
-  #   "Buzz"
-  # else 
-  #   nil
+  else if int % 5 == 0
+    "Buzz"
+  else 
+    nil
   end
 end 
